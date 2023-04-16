6652204f343bb195fbbfd612b0e3bec15c22c9c6
maxim <maksgamingcristalix@gmail.com>
2023-04-16 23:07:40 +0600

    Pagintation page 5 to 3

--- blogPosts/Blog/views.py
+++ blogPosts/Blog/views.py
@@ -24,7 +24,7 @@ class UserPostListView(ListView):
     model = Post
     template_name = 'Blog/user_posts.html'
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 3
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
